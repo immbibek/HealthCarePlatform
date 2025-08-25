a8662cf1a2745864560d78510dc0b1095ea66cf6
Bibek <137331749+immbibek@users.noreply.github.com>
2025-08-25 23:47:38 +0530

    App.jsx created

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./Context/AdminContext";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import { DoctorContext } from "./Context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointment from "./pages/Doctor/DoctorAppointment";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
-  return (
+  return aToken || dToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          {/* Admin Routes */}
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+
+          {/* Doctor Routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointment />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
